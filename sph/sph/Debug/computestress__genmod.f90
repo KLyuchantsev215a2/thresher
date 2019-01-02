6270754c4d766dcec8121bdeac79271d23a30680
Vladislav <klychik-nsk54rus@mail.ru>
2019-01-02 23:24:23 +0700

    Файл, напряжение
    Чтение с файлов данных о узлах, координаты и начальные скорости, пока без блока начальных условий. Функция расчета напряжения в материале, проверить деление на detF.

---
+++ sph/sph/Debug/computestress__genmod.f90
@@ -0,0 +1,13 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Jan 02 23:17:34 2019
+        MODULE COMPUTESTRESS__genmod
+          INTERFACE 
+            FUNCTION COMPUTESTRESS(F,C,MU,K,N)
+              INTEGER(KIND=4) :: N
+              REAL(KIND=4) :: F(2,2,N)
+              REAL(KIND=4) :: C(2,2,N)
+              REAL(KIND=4) :: MU
+              REAL(KIND=4) :: K
+              REAL(KIND=4) :: COMPUTESTRESS
+            END FUNCTION COMPUTESTRESS
+          END INTERFACE 
+        END MODULE COMPUTESTRESS__genmod
