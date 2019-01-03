d0af1beb6f5ef5db4c87b07809831f8c1401756f
Vladislav <klychik-nsk54rus@mail.ru>
2019-01-03 22:43:55 +0700

    ComputeF
    Добавлена функция вычисления тензора деформации.

---
+++ sph/sph/Debug/computef__genmod.f90
@@ -0,0 +1,14 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Jan 03 22:40:30 2019
+        MODULE COMPUTEF__genmod
+          INTERFACE 
+            FUNCTION COMPUTEF(VOL,X,X_OLD,NABLA_W_0,N,F)
+              INTEGER(KIND=4) :: N
+              REAL(KIND=4) :: VOL(N)
+              REAL(KIND=4) :: X(2,N)
+              REAL(KIND=4) :: X_OLD(2,N)
+              REAL(KIND=4) :: NABLA_W_0(2,N,N)
+              REAL(KIND=4) :: F(2,2,N)
+              REAL(KIND=4) :: COMPUTEF
+            END FUNCTION COMPUTEF
+          END INTERFACE 
+        END MODULE COMPUTEF__genmod
